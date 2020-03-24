95d83679f174e97bdf4912ee086d6d94f3f8ff17
Res Yeg <shaqk702@gmail.com>
2020-03-24 13:40:37 -0700

    Run some tests

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,14 @@ module.exports = {
       password: 'Password123',
       database: 'test'
     }
+  },
+  test: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: 'Password123',
+      database: 'test'
+    }
   }
 }
