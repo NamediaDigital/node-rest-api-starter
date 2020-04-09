aadd00e9d1436ef78f25815d6a88b0b9db52d4ab
Res Yeg <shaqk702@gmail.com>
2020-04-08 19:07:41 -0700

    Remove node start script

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var port = process.env.PORT || 8000;
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
