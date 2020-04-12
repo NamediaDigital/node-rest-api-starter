9c9ce5c6c4329f42bbb6a856b79c53cfd65ce1b5
Res Yeg <shaqk702@gmail.com>
2020-04-11 21:33:37 -0700

    Initial setup for sequelize cli tool

---
+++ db/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USER,
+    password: process.env.DEV_DB_PASS,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  },
+  test: {
+    username: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASS,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DIALECT,
+  },
+};
