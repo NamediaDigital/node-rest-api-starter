67f565f741d0fb43cade191b7309058daff96e2c
Res Yeg <shaqk702@gmail.com>
2020-04-13 23:09:00 -0700

    Add GET and POST methods

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../../db/models/user');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.status(200).send('respond with a resource');
+  User.findAll().then((users) => res.status(200).json(JSON.stringify(users)));
+});
+
+router.post('/', function (req, res, next) {
+  const newUser = req.body;
+  User.create(newUser)
+    .then((user) => res.status(201).json(JSON.stringify(user)))
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
