3991aaf5aef86a2b9bd7e8d4e9daba280a7c09b7
Res Yeg <shaqk702@gmail.com>
2020-04-13 23:08:14 -0700

    Fix model defintion

--- db/models/user.js
+++ db/models/user.js
@@ -1,13 +1,22 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../db/connection');
+
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {}
+  );
+  User.associate = function (models) {
     // associations can be defined here
   };
   return User;
 };
+
+module.exports = User(sequelize, DataTypes);
