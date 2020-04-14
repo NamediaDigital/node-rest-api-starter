5553585760cea556b9dd1ba61acc0584a8f271e0
Res Yeg <shaqk702@gmail.com>
2020-04-13 23:07:43 -0700

    Remove extra slash

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/v1/', indexRouter);
+app.use('/v1', indexRouter);
 app.use('/v1/users', usersRouter);
 
 // Database connection
