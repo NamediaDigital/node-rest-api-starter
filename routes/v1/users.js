8900925abc16582c01a40e4587dceefebecf35c1
Res Yeg <shaqk702@gmail.com>
2020-04-13 23:12:18 -0700

    Send JSON response

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -4,13 +4,15 @@ const User = require('../../db/models/user');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  User.findAll().then((users) => res.status(200).json(JSON.stringify(users)));
+  User.findAll()
+    .then((users) => res.status(200).json(users))
+    .catch((err) => console.log(err));
 });
 
 router.post('/', function (req, res, next) {
   const newUser = req.body;
   User.create(newUser)
-    .then((user) => res.status(201).json(JSON.stringify(user)))
+    .then((user) => res.status(201).json(user))
     .catch((err) => console.log(err));
 });
 
