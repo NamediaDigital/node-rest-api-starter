cc4d55c2f34b327bb3851b6e376ed68ef5ca9b33
Res Yeg <shaqk702@gmail.com>
2020-05-02 16:09:28 -0700

    Update comment

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* GET request handler for temporary home page. */
 router.get("/", function (req, res, next) {
   res.status(200).send("<h1>Express app</h1>");
 });
